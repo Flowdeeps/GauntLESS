4e1476c43132885284b811b94e90c4e00dc79c21
Flowdeeps <pete@republicofpete.net>
2013-03-06 19:57:38 +0100

    Stage zoom function

--- game/js/gauntless.js
+++ game/js/gauntless.js
@@ -12,6 +12,42 @@
 // functions with a scope restricted to the window load go here
 window.onload = function() {
 
+  // zoom target
+  var $stage = document.getElementById('stage');
+  var $zoomLess = document.getElementsByClassName('less')[0];
+  var $zoomMore = document.getElementsByClassName('more')[0];
+  var zoomScale = parseInt($stage.style.zoom);
+
+  // cheat zoom
+  if (document.getElementsByClassName('less')[0]){
+    $zoomLess.onclick = function(){
+      // make the stage 200% smaller on each click until it's minimum 100% of original scale
+      if (zoomScale == 1){
+        // do nowt
+      } else {
+        zoomScale -= 1;
+        $stage.style.zoom = zoomScale;
+      }
+      console.log(zoomScale);
+      return false;
+    }
+  }
+
+  if (document.getElementsByClassName('more')[0]){
+
+    $zoomMore.onclick = function(){
+      // make the stage 200% bigger on each click until it's maximum 600% of original scale
+      if (zoomScale >= 7){
+        // do nowt
+      } else {
+        zoomScale += 1;
+        $stage.style.zoom = zoomScale;
+      }
+      console.log(zoomScale);
+      return false;
+    }
+  }
+
   // map parser
   
 }
