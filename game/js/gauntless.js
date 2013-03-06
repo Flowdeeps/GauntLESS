7d5ffbdaf631c3acbc7a1d9d77f748a84023591b
Flowdeeps <pete@republicofpete.net>
2013-03-06 14:44:55 +0100

    Initial JS commit

--- game/js/gauntless.js
+++ game/js/gauntless.js
@@ -5,3 +5,18 @@
 *
 * =================================================
 */
+
+// functions with a global scope should go here
+
+
+// functions with a scope restricted to the window load go here
+window.onload = function() {
+
+  // map parser
+  
+}
+
+// functions with a scope restricted to the window unload go here - I'm thinking any sort of clean up at this stage
+window.unload = function(){
+  // hammer away at the keys here
+}
