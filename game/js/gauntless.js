d9ca5f6ff0dde19e54b776bd2e09fd5eb6711ed6
Mark Lewis <thnkloud9@gmail.com>
2013-03-07 03:07:50 +0100

    FIRING MOFOS

--- game/js/gauntless.js
+++ game/js/gauntless.js
@@ -138,24 +138,31 @@ window.onload = function() {
     "#aa00aa"
   ];
 
-	$mapArray = mapParse(map);
+  $mapArray = mapParse(map);
 
-	function colourLookup(x,y) {
-		colourCode = $colours[parseInt($mapArray[y][x],16)];
-		return colourCode;
-	}
+  function colourLookup(x,y) {
+    if (($mapArray[y][x] === "N") ||
+        ($mapArray[y][x] === "E") ||
+        ($mapArray[y][x] === "S") ||
+        ($mapArray[y][x] === "W")) {
+        var colour = 3;
+    } else {
+        var colour = parseInt($mapArray[y][x],16);
+    }
+    return $colours[colour];
+  }
 
-	function mapParse(map){
-	  var mapArray = [];
-	  var tmpArray = map.split("\n");
-	  for(var i = 0; i < tmpArray.length; i++) {
-	    mapArray.push(tmpArray[i].split(""));
-	  }
-		return mapArray;
+  function mapParse(map){
+    var mapArray = [];
+	var tmpArray = map.split("\n");
+    for(var i = 0; i < tmpArray.length; i++) {
+      mapArray.push(tmpArray[i].split(""));
 	}
+    return mapArray;
+  }
 
   function renderScreen() {
-    for(y = 0; y < $mapArray.length; y++) {
+    for(var y = 0; y < $mapArray.length; y++) {
       for(var x = 0; x < $mapArray[y].length; x++) {
         ctx.fillStyle = colourLookup(x,y);
         ctx.fillRect(x,y,1,1);
@@ -163,45 +170,61 @@ window.onload = function() {
     }
   }
 
-  // player movement functions
+  /**
+   * player movement functions
+   * and status vars
+   */
   var moving = false;
+  var facing = "N";
+  var playerSpeed = 20;
 
   function movePlayerUp() {
     stopMoving();
+    facing = "N";
     moving = setInterval(function()  {
       goUp();
-    }, 50);
+    }, playerSpeed);
   }
   function movePlayerDown() {
     stopMoving();
+    facing = "S";
     moving = setInterval(function()  {
       goDown();
-    }, 50);
+    }, playerSpeed);
   }
   function movePlayerLeft() {
     stopMoving();
+    facing = "W";
     moving = setInterval(function()  {
       goLeft();
-    }, 50);
+    }, playerSpeed);
   }
   function movePlayerRight() {
     stopMoving();
+    facing = "E";
     moving = setInterval(function()  {
       goRight();
-    }, 50);
+    }, playerSpeed);
   }
 
-  function goUp() {
+  function findPlayer() {
     for(var y = 0; y < $mapArray.length; y++) {
       for(var x = 0; x < $mapArray[y].length; x++) {
         if ($mapArray[y][x] === "F") {
           var playerX = x;
           var playerY = y;
-          break;
+          return([playerX, playerY]);
         }
       }
     }
-    nextPos = (playerY-1);
+
+  }
+
+  function goUp() {
+    var playerPos = findPlayer();
+    var playerX = playerPos[0];
+    var playerY = playerPos[1];
+    var nextPos = (playerY-1);
     if ($mapArray[nextPos][playerX] != 1) {
       $mapArray[nextPos][playerX] = "F";
       $mapArray[playerY][playerX] = 0;
@@ -209,16 +232,10 @@ window.onload = function() {
   }
 
   function goDown() {
-    for(var y = 0; y < $mapArray.length; y++) {
-      for(var x = 0; x < $mapArray[y].length; x++) {
-        if ($mapArray[y][x] === "F") {
-          var playerX = x;
-          var playerY = y;
-          break;
-        }
-      }
-    }
-    nextPos = (playerY+1);
+    var playerPos = findPlayer();
+    var playerX = playerPos[0];
+    var playerY = playerPos[1];
+    var nextPos = (playerY+1);
     if ($mapArray[nextPos][playerX] != 1) {
       $mapArray[nextPos][playerX] = "F";
       $mapArray[playerY][playerX] = 0;
@@ -226,16 +243,10 @@ window.onload = function() {
   }
 
   function goLeft() {
-    for(var y = 0; y < $mapArray.length; y++) {
-      for(var x = 0; x < $mapArray[y].length; x++) {
-        if ($mapArray[y][x] === "F") {
-          var playerX = x;
-          var playerY = y;
-          break;
-        }
-      }
-    }
-    nextPos = (playerX-1);
+    var playerPos = findPlayer();
+    var playerX = playerPos[0];
+    var playerY = playerPos[1];
+    var nextPos = (playerX-1);
     if ($mapArray[playerY][nextPos] != 1) {
       $mapArray[playerY][nextPos] = "F";
       $mapArray[playerY][playerX] = 0;
@@ -243,48 +254,115 @@ window.onload = function() {
   }
 
   function goRight() {
-    for(var y = 0; y < $mapArray.length; y++) {
-      for(var x = 0; x < $mapArray[y].length; x++) {
-        if ($mapArray[y][x] === "F") {
-          var playerX = x;
-          var playerY = y;
-          break;
-        }
-      }
-    }
-    nextPos = (playerX+1);
+    var playerPos = findPlayer();
+    var playerX = playerPos[0];
+    var playerY = playerPos[1];
+    var nextPos = (playerX+1);
     if ($mapArray[playerY][nextPos] != 1) {
       $mapArray[playerY][nextPos] = "F";
       $mapArray[playerY][playerX] = 0;
     }
   }
 
+  function fire() {
+    var playerPos = findPlayer();
+    var playerX = playerPos[0];
+    var playerY = playerPos[1];
+    if (facing === "N") {
+      var bulletPos = (playerY-1);
+      $mapArray[bulletPos][playerX] = "N"; 
+    } else if (facing === "S") {
+      var bulletPos = (playerY+1);
+      $mapArray[bulletPos][playerX] = "S"; 
+    } else if (facing === "E") {
+      var bulletPos = (playerX+1);
+      $mapArray[playerY][bulletPos] = "E"; 
+    } else if (facing === "W") {
+      var bulletPos = (playerX-1);
+      $mapArray[playerY][bulletPos] = "W"; 
+    }
+  
+  }
+
   function stopMoving() {
     clearInterval(moving);
   }
 
-  // move player
+  // move player key event handlers
   setHandler("Left", movePlayerLeft, stopMoving);
   setHandler("Right", movePlayerRight, stopMoving);
   setHandler("Up", movePlayerUp, stopMoving);
   setHandler("Down", movePlayerDown, stopMoving);
+  setHandler("Space", fire);
+  /**
+   * end player movement code
+   */
+
+  /**
+   * bullet code
+   */
+  var bullets = [];
+  var bulletChars = [ "N", "E", "S", "W" ];
+  function renderBullets() {
+    for(var y = 0; y < $mapArray.length; y++) {
+      for(var x = 0; x < $mapArray[y].length; x++) {
+        if (bulletChars.indexOf($mapArray[y][x]) >= 0) {
+          var bullet = {
+            "direction": $mapArray[y][x],
+            "x": x,
+            "y": y 
+          }
+          bullets.push(bullet);
+        }
+      } 
+    }
+
+    // handler n bullet
+    for (var b in bullets) {
+      var bullet = bullets[b];
+
+      if (bullet.direction == "N") {
+        var nextY = (bullet.y-1);
+        var nextX = bullet.x;
+      }
+      if (bullet.direction == "S") {
+        var nextY = (bullet.y+1);
+        var nextX = bullet.x;
+      }
+      if (bullet.direction == "E") {
+        var nextY = bullet.y;
+        var nextX = (bullet.x+1);
+      }
+      if (bullet.direction == "W") {
+        var nextY = bullet.y;
+        var nextX = (bullet.x-1);
+      }
+
+      if ($mapArray[nextY][nextX] != 1) {
+        // TODO check if we hit a player or enemy
+        $mapArray[nextY][nextX] = bullet.direction;
+        $mapArray[bullet.y][bullet.x] = 0;  
+      } else {
+        // bullet hit a wall
+        $mapArray[bullet.y][bullet.x] = 0;  
+      }
+    }
+ 
+  }
 
   var z = 1
   // main game loop, redraws every 50 milliseconds
   setInterval(function() {
+    renderBullets();
     renderScreen();
 
     // this is a test element
     // OMG IT BLINKS!!
     ctx.fillStyle = $colours[z];
     ctx.fillRect(5,5,1,1);
-    if(z==16){
-      z = 1;
-    } else {
-      z++;
-    }
+    if(z==16){ z = 1; } else { z++; }
 
-  }, 50);
+  }, 20);
 }
 
 // functions with a scope restricted to the window unload go here - I'm thinking any sort of clean up at this stage
